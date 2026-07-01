7106798ee6da9d0947c43dcff66427605e0c4fee
midnite-ops <charlesoyims@gmail.com>
2026-07-01 15:00:25 +0100

    Created the utils.js file

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,23 +2,23 @@ import { PenBoxIcon, Trash2Icon } from 'lucide-react'
 
 
 
-const NoteCard = () => {
+const NoteCard = ({ title, content, created }) => {
   return (
     <div className='bg-white p-6 rounded-lg'>
         <div className=' mb-7 flex gap-3 flex-col items-start'>
             <div className='flex flex-col gap-2'>
                 <h1 className='text-xl font-bold'>Title</h1>
-                <h1>My name is jesse Oyims</h1>
+                <h1>{title}</h1>
             </div>
             <div className='flex flex-col gap-2'>
                 <h1 className='text-lg font-bold text-black/40'>Content</h1>
-                <p>I am a super star</p>
+                <p>{content}</p>
             </div>
             
         </div>
 
         <div className='flex justify-between items-center'>
-            <p>Created at: 2023-10-01</p>
+            <p>Created at: {created}</p>
             <div className='flex gap-2 items-center'>
                 <PenBoxIcon />
                 <Trash2Icon color='red'/>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import NoteCard from '../components/NoteCard'
-import api from '../lib/axios.js'
-import { useEffect } from 'react';
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import NoteCard from "../components/NoteCard";
+import api from "../lib/axios.js";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const HomePage = () => {
-    const [notes, setNotes] = React.useState([]);
+  const [notes, setNotes] = React.useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const fetchNotes = async () => {
-        const res = await api.get("/notes");
-        setNotes(res.data);
+  const fetchNotes = async () => {
+    setLoading(true);
+    try {
+      const res = await api.get("/notes");
+      setNotes(res.data);
+    } catch (error) {
+      console.log("error in the fetchNotes function", error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        fetchNotes()
-    },[])
+  useEffect(() => {
+    fetchNotes();
+   
+  }, []);
+   console.log(notes)
+
   return (
-    <section className='bg-black h-screen w-full'>
-        <Navbar />
-        
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-30 px-10'>
-            {[1,2].map((note, index) => (
-                <NoteCard key={index} />
-            ))}
-        </div>
-    </section>
-  )
-}
+    <section className="bg-black h-screen w-full">
+      <Navbar />
 
-export default HomePage
+      {loading ? (
+        <div className="text-white">Loading notes.....</div>
+      ) : notes.length === 0 ? (
+        <div className="text-white text-2xl font-bold text-center mt-20">
+          No notes found
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-30 px-10">
+          {notes.map((note, index) => (
+            <NoteCard key={index} title={note.title} content={note.content} created={note.createdAt} />
+          ))}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default HomePage;
