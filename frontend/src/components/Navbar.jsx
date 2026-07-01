621192d969b066d9a8c2c6cd22a0e0f5d5982fd4
midnite-ops <charlesoyims@gmail.com>
2026-07-01 13:52:51 +0100

    Created the pages for the project

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div>ThinkerBell</div>
+        <div className='bg-white px-3 py-2'>New Note</div>
+    </header>
+  )
+}
+
+export default Navbar
